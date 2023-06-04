ffd3bdd33f203982b90f1d447f6be56a48aa160b
Admin <Admin@LENOVO>
2023-06-04 16:08:42 +0600

    Добавьте файлы проекта.

---
+++ PrataC.Preprocessor/const.h
@@ -0,0 +1,4 @@
+#define BEEP '\007'
+#define ESS 'S'
+#define NULL '\0'
+#define OOPS "So, you done it"
---
+++ PrataC.Preprocessor/stdio.h
@@ -0,0 +1,5 @@
+#define takein scanf_s
+#define whole char
+#define TWO 2
+#define start {
+#define end }
